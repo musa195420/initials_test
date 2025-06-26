6ed13f79bc65ba8a47437f3ee6b65d876b39ecc2
musa195420 <musa195420@gmail.com>
2025-06-26 11:07:58 +0500

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
